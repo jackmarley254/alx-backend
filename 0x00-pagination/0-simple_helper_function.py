8342c5087a6420882240e09d7a2391699f082bcb
jackmarley254 <engndiritu1@gmail.com>
2024-05-20 15:16:27 +0300

    Simple helper function corrections

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""function named index_range that takes two integer arguments
+page and page_size."""
 
-def index_range(page, page_size):
+
+def function(page, page_size):
     """
     Calculate the start and end indices of a range given a page number
     and page size.
